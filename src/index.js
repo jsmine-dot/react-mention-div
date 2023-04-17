e680d168b2107aeaf6692cb5cd07c906407f0158
Manish Kumar <manish1993singh@gmail.com>
2023-04-17 23:11:51 +0530

    additional attribute will  be passed to mention div (#7)

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React, { useEffect, useRef } from "react";
 import observer from "@jsmine/mention-observer";
 
-export default function MentionBox(props) {
+export default function MentionBox({ onChange, options, value, ...props }) {
     const ref = useRef(null);
-    useEffect(() => { ref.current && observer.set({ editArea: ref.current }) }, [ref.current])
-    useEffect(() => { props.onChange && observer.set({ callback: (data) => props.onChange({ ...data }) }) }, [props.onChange]);
-    useEffect(() => { props.options && observer.set({ options: props.options }) }, [props.options]);
-    useEffect(() => { props.value && observer.set({ value: props.value }) }, [props.value]);
+    useEffect(() => { ref.current && observer.set({ editArea: ref.current }) }, [ref.current]);
+    useEffect(() => { onChange && observer.set({ callback: (data) => onChange({ ...data }) }) }, [onChange]);
+    useEffect(() => { options && observer.set({ options: options }) }, [options]);
+    useEffect(() => { value && observer.set({ value: value }) }, [value]);
     return <div
-        className={props.className}
+        {...props}
         contentEditable={true}
         ref={ref}
-        style={props.style}
     />
 }
